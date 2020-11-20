fe758037558ab8880ee8bb1b9754c7ea64973315
shinyay <shinya.com@gmail.com>
2020-11-20 14:54:19 +0900

    Add spring-cloud-gcp-starter-security-iap for Cloud Identity Aware Proxy

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.cloud:spring-cloud-gcp-starter")
+	implementation("org.springframework.cloud:spring-cloud-gcp-starter-security-iap")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
