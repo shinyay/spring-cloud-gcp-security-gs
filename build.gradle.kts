0f8298aa0714a59b2b98f057a5a2179a10c06a7b
shinyay <shinya.com@gmail.com>
2020-11-20 18:14:57 +0900

    Add AppEngine plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.4.0"
 	id("io.spring.dependency-management") version "1.0.10.RELEASE"
+	id("com.google.cloud.tools.appengine") version "2.4.1"
 	kotlin("jvm") version "1.4.10"
 	kotlin("plugin.spring") version "1.4.10"
 }
