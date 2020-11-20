60a82416e393363888f17eb8a3b0b25893ba3a16
shinyay <shinya.com@gmail.com>
2020-11-20 17:24:45 +0900

    Add default access function

--- src/main/kotlin/com/google/shinyay/controller/HelloController.kt
+++ src/main/kotlin/com/google/shinyay/controller/HelloController.kt
@@ -1,7 +1,19 @@
 package com.google.shinyay.controller
 
+import com.google.shinyay.logger
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 @RestController
 class HelloController {
+
+    @GetMapping("/")
+    fun defaultAccess(): String {
+        val dateAndTime = ZonedDateTime.now(ZoneId.of("Japan")).format(DateTimeFormatter.ofPattern("yyyy-MM-dd: HH:mm:ss"))
+        logger.info(dateAndTime)
+        return "Hello at $dateAndTime"
+    }
 }
