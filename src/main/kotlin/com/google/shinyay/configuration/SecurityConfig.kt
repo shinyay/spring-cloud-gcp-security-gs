398eccf9aba77c24a26ed25278eaf8a8c55f57c6
shinyay <shinya.com@gmail.com>
2020-11-20 18:00:36 +0900

    Add authentication configuration function

--- src/main/kotlin/com/google/shinyay/configuration/SecurityConfig.kt
+++ src/main/kotlin/com/google/shinyay/configuration/SecurityConfig.kt
@@ -1,8 +1,19 @@
 package com.google.shinyay.configuration
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint
 
 @EnableWebSecurity
 class SecurityConfig : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity?) {
+        http?.authorizeRequests()
+                ?.antMatchers("/")?.authenticated()
+                ?.and()
+                ?.oauth2ResourceServer()?.jwt()
+                ?.and()
+                ?.authenticationEntryPoint(Http403ForbiddenEntryPoint())
+    }
 }
