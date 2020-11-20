7630176f9c230b03add205d00591af1f76e7614c
shinyay <shinya.com@gmail.com>
2020-11-20 17:36:27 +0900

    Add WebSecurityConfiguration Class for Spring Security

---
+++ src/main/kotlin/com/google/shinyay/configuration/SecurityConfig.kt
@@ -0,0 +1,7 @@
+package com.google.shinyay.configuration
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+
+@EnableWebSecurity
+class SecurityConfig {
+}
