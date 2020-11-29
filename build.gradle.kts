09d036ca21f8b5e8e56b05ff09fb1a3ef3e6c0a6
shinyay <shinya.com@gmail.com>
2020-11-30 08:40:56 +0900

    Add Jib Plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.10.RELEASE"
 	kotlin("jvm") version "1.4.10"
 	kotlin("plugin.spring") version "1.4.10"
+	id("com.google.cloud.tools.jib") version "2.6.0"
 }
 
 group = "com.google.shinyay"
